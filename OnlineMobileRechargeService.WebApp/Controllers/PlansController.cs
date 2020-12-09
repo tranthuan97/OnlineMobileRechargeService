73852fac7b83252dfeb2da7a459155d862cb4add
Phan Ngoc Lam <lampn@ames.edu.vn>
2020-12-09 13:12:55 +0700

    update plans controller

--- OnlineMobileRechargeService.WebApp/Controllers/PlansController.cs
+++ OnlineMobileRechargeService.WebApp/Controllers/PlansController.cs
@@ -1,166 +1,55 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using OnlineMobileRechargeService.Data.EF;
 using OnlineMobileRechargeService.Data.Entities;
 
 namespace OnlineMobileRechargeService.WebApp.Controllers
 {
-    public class PlansController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlansController : ControllerBase
     {
-        private readonly OMRSDbContext _context;
+        private readonly OMRSDbContext _dbContext;
 
-        public PlansController(OMRSDbContext context)
+        public PlansController(OMRSDbContext dbContext)
         {
-            _context = context;
+            _dbContext = dbContext;
         }
 
-        // GET: Plans
-        public async Task<IActionResult> Index()
+        // GET: api/Plans
+        [HttpGet]
+        public ActionResult<IEnumerable<Plan>> Get()
         {
-            var oMRSDbContext = _context.Plans.Include(p => p.Provider).Include(p => p.VAS);
-            return View(await oMRSDbContext.ToListAsync());
+            return _dbContext.Plans.ToList();
         }
 
-        // GET: Plans/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: api/Plans/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var plan = await _context.Plans
-                .Include(p => p.Provider)
-                .Include(p => p.VAS)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (plan == null)
-            {
-                return NotFound();
-            }
-
-            return View(plan);
+            return "value";
         }
 
-        // GET: Plans/Create
-        public IActionResult Create()
-        {
-            ViewData["ProviderId"] = new SelectList(_context.Providers, "Id", "Logo");
-            ViewData["VASId"] = new SelectList(_context.VAS, "Id", "Name");
-            return View();
-        }
-
-        // POST: Plans/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: api/Plans
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,ProviderId,VASId,Amount,Price,Validate,Description")] Plan plan)
+        public void Post([FromBody] string value)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(plan);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ProviderId"] = new SelectList(_context.Providers, "Id", "Logo", plan.ProviderId);
-            ViewData["VASId"] = new SelectList(_context.VAS, "Id", "Name", plan.VASId);
-            return View(plan);
         }
 
-        // GET: Plans/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        // PUT: api/Plans/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var plan = await _context.Plans.FindAsync(id);
-            if (plan == null)
-            {
-                return NotFound();
-            }
-            ViewData["ProviderId"] = new SelectList(_context.Providers, "Id", "Logo", plan.ProviderId);
-            ViewData["VASId"] = new SelectList(_context.VAS, "Id", "Name", plan.VASId);
-            return View(plan);
         }
 
-        // POST: Plans/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ProviderId,VASId,Amount,Price,Validate,Description")] Plan plan)
+        // DELETE: api/Plans/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
         {
-            if (id != plan.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(plan);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PlanExists(plan.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ProviderId"] = new SelectList(_context.Providers, "Id", "Logo", plan.ProviderId);
-            ViewData["VASId"] = new SelectList(_context.VAS, "Id", "Name", plan.VASId);
-            return View(plan);
-        }
-
-        // GET: Plans/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var plan = await _context.Plans
-                .Include(p => p.Provider)
-                .Include(p => p.VAS)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (plan == null)
-            {
-                return NotFound();
-            }
-
-            return View(plan);
-        }
-
-        // POST: Plans/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var plan = await _context.Plans.FindAsync(id);
-            _context.Plans.Remove(plan);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool PlanExists(int id)
-        {
-            return _context.Plans.Any(e => e.Id == id);
         }
     }
 }
