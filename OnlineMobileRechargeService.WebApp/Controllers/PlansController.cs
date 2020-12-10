db5dd12276540225b26afe69f01b6debaa9d3a1c
tranthuan.ttt@gmail.com <tranthuan.ttt@gmail.com>
2020-12-10 17:34:39 +0700

    update plan

--- OnlineMobileRechargeService.WebApp/Controllers/PlansController.cs
+++ OnlineMobileRechargeService.WebApp/Controllers/PlansController.cs
@@ -1,55 +1,121 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OnlineMobileRechargeService.Data.EF;
 using OnlineMobileRechargeService.Data.Entities;
 
 namespace OnlineMobileRechargeService.WebApp.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     [ApiController]
     public class PlansController : ControllerBase
     {
-        private readonly OMRSDbContext _dbContext;
+        private readonly OMRSDbContext _context;
 
-        public PlansController(OMRSDbContext dbContext)
+        public PlansController(OMRSDbContext context)
         {
-            _dbContext = dbContext;
+            _context = context;
         }
 
         // GET: api/Plans
         [HttpGet]
-        public ActionResult<IEnumerable<Plan>> Get()
+        public async Task<ActionResult<IEnumerable<Plan>>> GetPlans()
         {
-            return _dbContext.Plans.ToList();
+
+            Dictionary<string, Object> data = new Dictionary<string, object>();
+            data.Add("status", "SUCCESS");
+            data.Add("data", null);
+
+            var listPlans = await _context.Plans.ToListAsync();
+            
+            data.Remove("data");
+            data.Add("message", "Get data is success ! !");
+            data.Add("data", listPlans);
+
+            return Ok(data);
         }
 
         // GET: api/Plans/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Plan>> GetPlan(int id)
         {
-            return "value";
-        }
+            var plan = await _context.Plans.FindAsync(id);
 
-        // POST: api/Plans
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
+            if (plan == null)
+            {
+                return NotFound();
+            }
+
+            return plan;
         }
 
         // PUT: api/Plans/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> PutPlan(int id, Plan plan)
         {
+            if (id != plan.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(plan).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlanExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Plans
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Plan>> PostPlan(Plan plan)
+        {
+            _context.Plans.Add(plan);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPlan", new { id = plan.Id }, plan);
         }
 
         // DELETE: api/Plans/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> DeletePlan(int id)
         {
+            var plan = await _context.Plans.FindAsync(id);
+            if (plan == null)
+            {
+                return NotFound();
+            }
+
+            _context.Plans.Remove(plan);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PlanExists(int id)
+        {
+            return _context.Plans.Any(e => e.Id == id);
         }
     }
 }
